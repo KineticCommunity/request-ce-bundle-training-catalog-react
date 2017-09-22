c2e1b5f7d599389f50170cde42fa0bf827fbf9a7
chad.rehm <chad.rehm@CORP.KINETICDATA.COM>
2017-09-21 23:35:38 -0500

    removed category list contianer

--- src/components/Services/CategoryListContainer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { CategoryList } from './CategoryList';
-
-const mapStateToProps = state => ({
-  categories: state.categories.data,
-  forms: state.forms.data,
-});
-
-export const CategoryListContainer = connect(mapStateToProps)(CategoryList);
